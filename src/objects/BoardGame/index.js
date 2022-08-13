02e3c572d014098d7d8613f929b6a6aaff1526b2
alexsandrosantos35 <alexsandrosantos35@gmail.com>
2022-08-13 20:11:29 -0300

    Create PlayerName component without css

--- src/objects/BoardGame/index.js
+++ src/objects/BoardGame/index.js
@@ -5,8 +5,6 @@ function BoardGame (amountCards) {
 
     const $htmlCardGame = CardGame();
     const $htmlContent = $htmlCardGame.repeat(amountCards) 
-   
-    console.log($htmlContent);
 
 
     return `<section class="board-game">
