4a7e7328e5e2128b02507a5cefd4bc623ccf5c2c
alexsandrosantos35 <alexsandrosantos35@gmail.com>
2022-08-12 21:23:58 -0300

    Create component, first commit

---
+++ src/components/CardGame/index.js
@@ -0,0 +1,12 @@
+import './style.css';
+
+function CardGame() {
+    return`
+    <article class="card-game">
+    <img src="src/images/alura-pixel.png" alt="Logo da alura">
+    </article>
+    `;
+
+}
+
+export default CardGame;
