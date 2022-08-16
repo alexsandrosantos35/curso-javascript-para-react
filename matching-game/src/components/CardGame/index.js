741046cf1e0be6b4b31164f198488df2a1be4054
alexsandrosantos35 <alexsandrosantos35@gmail.com>
2022-08-15 23:53:45 -0300

    Introduce new feature, now is possible change the card icon.

--- matching-game/src/components/CardGame/index.js
+++ matching-game/src/components/CardGame/index.js
@@ -1,9 +1,9 @@
 import './style.css';
 
-function CardGame() {
+function CardGame(icon = "alura-pixel", alt = "Logo da alura") {
     return /*html*/`
     <article class="card-game">
-    <img src="src/images/alura-pixel.png" alt="Logo da alura">
+    <img src="src/images/${icon}.png" alt="${alt}">
     </article>
     `;
 
