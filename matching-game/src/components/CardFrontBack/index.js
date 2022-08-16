4952e8f77f92ad3697ce84ddf84923bcf4df9ca8
alexsandrosantos35 <alexsandrosantos35@gmail.com>
2022-08-16 20:22:58 -0300

    Introduce new feature, flip the card.

--- matching-game/src/components/CardFrontBack/index.js
+++ matching-game/src/components/CardFrontBack/index.js
@@ -1,12 +1,19 @@
 import CardGame from "../CardGame";
+import "./style.css";
 
 function CardFrontBack() {
-return /*html*/`
-<article>
-${CardGame()}
-${CardGame("javascript", "Logo do javascript")}
-</article>
-`;
+    return /*html*/ `
+    <article class="card-front-back">
+      
+      <div class="card -front">
+        ${CardGame()}
+      </div>
+      <div class="card -back">
+        ${CardGame("javascript", "Logo do JavaScript")}
+      </div>
+    </article>
+     
+  `;
 }
 
 export default CardFrontBack;
