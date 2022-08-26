5c1b258f027da1998aa6e63270c9299bc5a67064
alexsandrosantos35 <alexsandrosantos35@gmail.com>
2022-08-26 18:36:23 -0300

    Work in progress, create the ArroweeDown component without css

---
+++ matching-game/src/components/ArrowDown/index.js
@@ -0,0 +1,8 @@
+function ArrowDown() {
+    return /*html*/ `
+    <img src="src/images/IconArrowDown.png" alt="Icone de seta para baixo">
+    `;
+
+} 
+
+export default ArrowDown
--- matching-game/src/objects/ScoreBoard/index.js
+++ matching-game/src/objects/ScoreBoard/index.js
@@ -2,10 +2,12 @@ import PlayerName from "../../components/PlayerName";
 import './style.css';
 import VsPlayer from "../../components/VsPlayer";
 import PlayerScore from "../../components/PlayerScore";
+import ArrowDown from "../../components/ArrowDown";
 
 function ScoreBoard() {
 return /*html*/` 
 <header class="score-board">
+${(ArrowDown())}
 ${PlayerName("Player1")}
 ${PlayerScore(0)}
 ${VsPlayer("vs")}
