8f642d6d8f402dd1e8d2c2ae048c66fe6827519e
alexsandrosantos35 <alexsandrosantos35@gmail.com>
2022-08-26 17:24:40 -0300

    Add and update UI and style files, create style of the PlayerScore

--- matching-game/src/objects/ScoreBoard/index.js
+++ matching-game/src/objects/ScoreBoard/index.js
@@ -7,8 +7,9 @@ function ScoreBoard() {
 return /*html*/` 
 <header class="score-board">
 ${PlayerName("Player1")}
-${PlayerScore(1)}
+${PlayerScore(0)}
 ${VsPlayer("vs")}
+${PlayerScore(1)}
 ${PlayerName("Player2")}
 </header>
 `;
